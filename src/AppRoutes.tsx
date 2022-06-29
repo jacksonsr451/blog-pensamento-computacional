8f0d462cd27c091cde9e860e5be236bfd352d5f2
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-06-29 15:17:30 -0300

    Update components

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,11 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import LandingPage from './Pages/LandingPage'
+
 
 export default function AppRoutes() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<LandingPage/>} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path='/' element={<LandingPage/>} />
+        </Routes>
     )
 }
--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -1,5 +1,18 @@
+import { Helmet } from "react-helmet";
+import Footer from "../../Components/Footer";
+import Header from "../../Components/Header";
+
 export default function LandingPage() {
+
+
     return (
-        <></>
+        <div>
+            <Helmet>
+                <title>Landing Page</title>
+                <meta name="description" content="Lorem ipsum dolor sit amet" />
+            </Helmet>
+            <Header />
+            <Footer />
+        </div>
     )
 }
